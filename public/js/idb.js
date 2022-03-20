a7862a42b20abb6d85184e5bfa26dfdac9ddf2e9
IanAdamPrice <ianadamprice@gmail.com>
2022-03-20 09:53:02 -0400

    bug with idb.js

--- public/js/idb.js
+++ public/js/idb.js
@@ -12,17 +12,56 @@ request.onsuccess = function(event) {
   db = event.traget.result;
 
   if (navigator.onLine) {
-    // TODO
+    budgetObjectStore();
   }
 };
 
 request.onerror = function(event) {
-  // TODO
   console.log(event.target.errorCode);
 };
 
 function saveRecord(record) {
   //TODO
+  const transaction = db.transaction(['new_budget'], 'readwrite');
 
+  const budgetObjectStore = transaction.objectStore('new_budget');
 
+  budgetObjectStore.add(record);
 }
+
+function uploadBudget() {
+  const transaction = db.transaction(['new_budget'], 'readwrite');
+
+  const budgetObjectStore = transaction.objectStore('new_budget');
+
+  const getAll= budgetObjectStore.getAll();
+
+  getAll.onsuccess = function() {
+    if (getAll.result.length > 0) {
+      fetch('/api/transactions', {
+        method: 'POST',
+        body: JSON.stringify(getAll.result),
+        headers: {
+          Accept: 'application/json, text/plain, */*',
+          'Content-Type': 'application/json'
+        }
+      })
+        .then(response => response.json())
+        .then(serverResponse => {
+          if (serverResponse.message) {
+            throw new Error(serverResponse);
+          }
+
+          const transaction = db.transaction(['new_budget'], 'readwrite');
+          const budgetObjectStore = transaction.objectStore('new_budget');
+          budgetObjectStore.clear();
+        })
+        .catch(err => {
+          console.log(err);
+        });
+    }
+  };
+}
+
+window.addEventListener('online', uploadBudget);
+
--- public/service-worker.js
+++ public/service-worker.js
@@ -1,9 +1,13 @@
+const APP_PREFIX = 'BudgetTracker-';
+const VERSION = 'version_01';
+const CACHE_NAME = APP_PREFIX + VERSION;
+
 const FILES_TO_CACHE = [
-  "./index.html",
-  "./manifest.js",
-  "./js/idb.js",
-  "./js/index.js",
-  "./css/style.css",
+  "index.html",
+  "manifest.json",
+  "js/idb.js",
+  "js/index.js",
+  "css/styles.css",
   // "./icons/icon-72x72.png",
   // "./icons/icon-96x96.png",
   // "./icons/icon-128x128.png",
@@ -14,15 +18,46 @@ const FILES_TO_CACHE = [
   // "./icons/icon-512x512.png",
 ];
 
-const APP_PREFIX = 'BudgetTracker-';
-const VERSION = 'version_01';
-const CACHE_NAME = APP_PREFIX + VERSION;
-
 self.addEventListener('install', function (e) {
   e.waitUntil(
     caches.open(CACHE_NAME).then(function (cache) {
       console.log('installing cache : ' + CACHE_NAME)
       return cache.addAll(FILES_TO_CACHE)
+    })
+  )
+})
+
+self.addEventListener('activate', function (e) {
+  e.waitUntil(
+    caches.keys().then(function(keyList) {
+      let cacheKeeplist = keyList.filter(function (key) {
+        return key.indexOf(APP_PREFIX);
+      })
+      cacheKeeplist.push(CACHE_NAME);
+
+      return Promise.all(
+        keyList.map(function (key, i) {
+          if (cacheKepplist.indexOf(key) === -1) {
+            console.log('deleting cache : ' + keyList[i] );
+            return caches.delete(keyList[i]);
+          }
+        })
+      );
+    })
+  );
+});
+
+self.addEventListener('fetch', function(e) {
+  console.log('fetch request : ' + e.request.url)
+  e.respondWith(
+    caches.match(e.request).then(function (request) {
+      if (request) {
+        console.log('responding with cache : ' + e.request.url)
+        return request
+      } else {
+        console.log('file is not cached, fetching : ' + e.request.url)
+        return fetch(e.request)
+      }
     })
   )
 })
