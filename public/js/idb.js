1fb487cf632a794bd5cfffaed8b9511119669fe5
IanAdamPrice <ianadamprice@gmail.com>
2022-03-22 15:00:12 -0400

    bug fix

--- public/js/idb.js
+++ public/js/idb.js
@@ -2,7 +2,7 @@ let db;
 
 const request = indexedDB.open('budget_tracker', 1);
 
-request.onupgradeneeded = function(event) {
+request.onupgradeneeded = function (event) {
   const db = event.target.result;
 
   db.createObjectStore('new_transaction', {autoIncrement: true});
