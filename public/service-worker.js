c0a4bf26ce33a5934f6ef01d6bd0701fc30d7157
IanAdamPrice <ianadamprice@gmail.com>
2022-03-22 14:27:00 -0400

    minor fix

--- public/service-worker.js
+++ public/service-worker.js
@@ -8,14 +8,14 @@ const FILES_TO_CACHE = [
   "./js/idb.js",
   "./js/index.js",
   "./css/styles.css",
-  // "./icons/icon-72x72.png",
-  // "./icons/icon-96x96.png",
-  // "./icons/icon-128x128.png",
-  // "./icons/icon-144x144.png",
-  // "./icons/icon-152x152.png",
-  // "./icons/icon-192x192.png",
-  // "./icons/icon-384x384.png",
-  // "./icons/icon-512x512.png",
+  "./icons/icon-72x72.png",
+  "./icons/icon-96x96.png",
+  "./icons/icon-128x128.png",
+  "./icons/icon-144x144.png",
+  "./icons/icon-152x152.png",
+  "./icons/icon-192x192.png",
+  "./icons/icon-384x384.png",
+  "./icons/icon-512x512.png",
 ];
 
 self.addEventListener('install', function (e) {
@@ -56,7 +56,6 @@ self.addEventListener('fetch', function (e) {
         console.log('file is not cached, fetching : ' + e.request.url)
         return fetch(e.request)
       }
-
     })
   )
 })
