cbf40c01504aa32c09bf3d3c536cfd26e7a147a8
IanAdamPrice <ianadamprice@gmail.com>
2022-03-28 14:38:37 -0400

    deleted one letter, lets see if its fixed

--- public/js/idb.js
+++ public/js/idb.js
@@ -37,7 +37,7 @@ function checkDB() {
 
   getAll.onsuccess = function() {
     if (getAll.result.length > 0) {
-      fetch('/api/transactions', {
+      fetch('/api/transaction', {
         method: 'POST',
         body: JSON.stringify(getAll.result),
         headers: {
